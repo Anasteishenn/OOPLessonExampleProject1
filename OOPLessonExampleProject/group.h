091d0a91f81e8e7b60814f3923987ebb6a621354
Anasteishenn <anastasisuvorova1@gmail.com>
2023-04-05 18:46:55 +0300

    Add  interface of group class

---
+++ OOPLessonExampleProject/group.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "main.h"
+#include "student.h"
+#define DEFAUKT_SIZE 10
+
+class Group {
+private:
+
+	Student list[DEFAUKT_SIZE];
+	int size;
+	string name;
+
+public:
+	Group();
+	Group(Student* ls,int sz,string nm);
+	Group(string n);
+	~Group();
+
+	void add(Student student);
+	void remove(Student student);
+	void remove(int index);
+	Student get(int index);
+	int size();
+	string conver_to_string();
+};
